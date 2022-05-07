30a4caea7e8d31fc7f9a82bc9454f04b3cf231a7
SuperComputer29 <madhavbkrishna29@gmail.com>
2022-05-07 10:42:45 +0530

    more progress pwease!!

--- neural network_try4.py
+++ neural network_try4.py
@@ -8,11 +8,11 @@ import numpy as np
 training_data = np.array([[0, 1, 0], [1, 0, 1], [0, 1, 1], [1, 1, 1], [1, 0, 0], [1, 1, 0]])
 outputs = np.array([[1, 0], [1, 0], [0, 1], [0, 1], [1, 0], [1, 0]])
 
-a_len = len(training_data[0])
-z_len = 2
-A_len = 2 
-n = len(training_data)
-o_len = len(outputs[0]) 
+a_len = len(training_data[0]) # Number of input neruons
+z_len = 2 # Number of hidden layer neurons
+A_len = 2 # Number of output layer neurons
+n = len(training_data) # Number of training examples
+o_len = len(outputs[0]) # Number of expected outputs
 
 
 def init_weights_and_biases():
@@ -23,15 +23,15 @@ def init_weights_and_biases():
     
     return weights1, weights2, bias1, bias2
 
-def sigmoid(x):
+def sigmoid(x): # The first activation function that transforms the dot product into the hidden layer
     return 1/(1 + np.exp(-x))
 
 
-def sigmoid_derivative(x):
+def sigmoid_derivative(x): # The derivative of the sigmoid function
     return sigmoid(x)*(1 - sigmoid(x))
 
 
-def softmax(x):
+def softmax(x): # The second activation function
     exp_x = []
     for i in range(len(x)):
         exp_x.append(np.exp(x[i]))
@@ -48,16 +48,16 @@ def softmax_derivative(x):
     return softmax(x).dot(np.subtract(1, softmax(x)))
 
 
-def delistify(a):
+def delistify(a): # Converts a single element list into a string containing that element
     if len(a) == 1:
         return a[0]
     else:
-        return "it's not a single array list ;-;"
+        return "it's not a single element list ;-;"
 
 w1, w2, b1, b2 = init_weights_and_biases()
 
 
-def front_prop():
+def front_prop(): # Front propogation
     a = training_data
     z = np.array([[sigmoid(i[j] + b1[j][0]) for j in range(z_len)] for i in a.dot(w1.T)])
     # first compute A without putting it through the softmax function
@@ -68,7 +68,7 @@ def front_prop():
 z, A = front_prop() 
 
 
-def info():
+def info(): # I'll spit out information regarding the NN and also help you maintain your sanity
     print("    a      ")
     print("                  ")
     print(training_data)
@@ -97,21 +97,53 @@ def info():
     print("                  ")
     print(A) 
 
-def back_prop():
+def back_prop(): # Back propogation
     O = outputs
     a = training_data
-    cost = np.average([sum(i) for i in np.square(np.subtract(O, A))])
-    def z_gradient(k):
-        return np.array([[sigmoid_derivative(a[i].dot(w1[j].T) + float(b1[j]))*a[i][k] for j in range(z_len)] for i in range(n)])
+    cost2 = np.average([sum(i) for i in np.square(np.subtract(O, A))])
+    epochs = 1
+    learning_rate = 0.1
 
-    
-    dCdw = (-2/n)*sum([sum([(O[i][j] - A[i][j])*sigmoid_derivative(z[i])[j]*(z_gradient(0)[i].dot(w2[j].T)) for j in range(o_len)]) for i in range(n)])
+    # vectors
 
+    # w1_v = w1.reshape([a_len*z_len, 1])
+    # w2_v = w1.reshape([z_len*A_len, 1])
+    # b1_v = b1
+    # w1_v = b2
+
+    def w1(b, c, example):
+        return -2*(O-A)*softmax_derivative(z[example].dot(w2) + b2)*w1[b][c]*sigmoid_derivative(a[example].dot(w1) + b1)*w1[b][c]
+
+
+    def w2(b, c, example):
+        return -2*(O-A)*softmax_derivative(z[example].dot(w2) + b2)*w2[b][c]
+
+
+    def b1(b, example):
+        return -2*(O-A)*softmax_derivative(z[example].dot(w2) + b2)[b]*sigmoid_derivative(a[example].dot(w1[b]) + b1[b])
+
+
+    def b2(b, example):
+        return -2*(O-A)*softmax_derivative(z[example].dot(w2) + b2)[b]
+        
+
+    for k in range(epochs): # i is already taken :(
+        for example in range(n):
+            front_prop()
+            w1_change = [[w1(i, j, example) for j in range(a_len)] for i in range(z_len)]
+            w2_change = [[w2(i, j, example) for j in range(z_len)] for i in range(A_len)]
+            b1_change = [b1[i, example] for i in range(z_len)]
+            b2_change = [b2[i, example] for i in range(A_len)]
+
+            w1 -= learning_rate*w1_change
+            w2 -= learning_rate*w2_change
+            b1 -= learning_rate*b1_change
+            b2 -= learning_rate*b2_change
 
         
-    return cost, dCdw
+    return w1, w2, b1, b2
 
-cost, dCdw = back_prop()
+W1, W2, B1, B2 = back_prop()
 
 
 
